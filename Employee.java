4da9f65d71c5d0b7ddfcf6d9ee0c0e9bd5e2c9b9
Malagouda1863 <malangoudapatil7788@gmail.com>
2025-05-06 14:25:23 +0530

    save file

--- Employee.java
+++ Employee.java
@@ -3,7 +3,9 @@ public class Employee {
     String name;
     int id;
     double salary;
-    public Employee(String name, int id, double salary) {
+    int phone
+    public Employee(String name, int id, double salary,int phone) {
+        this.phone = phone;
         this.name = name;
         this.id = id;
         this.salary = salary;
@@ -12,9 +14,10 @@ public class Employee {
         System.out.println("Name: " + name);
         System.out.println("ID: " + id);
         System.out.println("Salary: " + salary);
+        System.out.println("Phone: " + phone);
     }
     public static void main(String[] args) {
-        Employee emp1 = new Employee("XYZ", 101, 50000.0);
+        Employee emp1 = new Employee("XYZ", 101, 50000.0, 1234567890);
         emp1.displayinfo();
     }
 }
